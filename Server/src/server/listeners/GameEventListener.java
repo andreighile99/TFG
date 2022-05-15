d7ad9a5f0175b67c9f563a75179a9f8254e9f450
Eduard Andrei Ghile <andreighile99@gmail.com>
2022-05-15 15:12:26 +0200

    Update the listeners

--- Server/src/server/listeners/GameEventListener.java
+++ Server/src/server/listeners/GameEventListener.java
@@ -9,7 +9,6 @@ public class GameEventListener extends Listener {
     @Override
     public void received(Connection connection, Object object) {
         if(object instanceof PositionEvent){
-            System.out.println("He recibido una nueva posición");
             PositionEvent positionEvent = (PositionEvent) object;
             //Get the lobby in which a player has moved, get the game server, get the map and update the player position
             LobbyHandler.INSTANCE.getLobbies().get(positionEvent.getLobyName()).getGameServer().getServerMap().updatePlayerPosition(positionEvent);
